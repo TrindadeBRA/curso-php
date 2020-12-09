f6e110ef62b3671fc8ab1cd03b07daeb34a6d762
Trindade <trindadebra@gmail.com>
2020-12-09 01:31:36 -0300

    Initial commit

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 ## Exemplos codados em aula.
 
-### Technologys:
+### Tecnologia:
 <img src="https://img.shields.io/badge/php-F7DF1E?style=for-the-badge&logo=php&logoColor=black" alt="PHP Badge" />
---
+++ strings/exemplo-01.php
@@ -0,0 +1,18 @@
+<?php
+
+$nome = "Lucas";
+$nome2 = 'Lucas';
+
+var_dump($nome);
+echo "<br>";
+var_dump($nome2);
+echo "<br>";
+
+//Interpolação de variaveis
+echo 'Meu nome é $nome';
+echo "<br>";
+echo "Meu nome é $nome";
+echo "<br>";
+
+
+?>
